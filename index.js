aa94be431af1d67a984e7c2fd6f7ff03ea1adc7f
osmanmrtacar <osmanmrtacar@yandex.com>
2022-05-04 19:57:30 +0300

    serve static html files

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const net = require('net');
+const fs = require('fs');
 
 const server = net.createServer();
+
+const handleRequest = async (path) => {
+  let requestedFile = `${path}.html`;
+  if (path === '/') {
+    requestedFile = '/index.html';
+  }
+  const fileData = await fs.promises.readFile(`.${requestedFile}`);
+  return fileData;
+};
 
 server.on('connection', (socket) => {
   console.log('client connected');
 
-  socket.on('data', (data) => {
-    console.log(data.toString());
+  socket.on('data', async (data) => {
+    const [requestHeader] = data.toString().split('\n\n');
+
+    const [requestLine] = requestHeader.split('\n');
+
+    const [method, path, httpVersion] = requestLine.split(' ');
+
+    const fileData = await handleRequest(path);
+
     const header = 'HTTP/1.1 200 OK\nServer: SimpleHTTPServer Nodejs\n\n';
     socket.write(header);
 
-    const body = 'Hello World';
-    socket.write(body);
+    socket.write(fileData);
 
     socket.end();
   });
